81f1dd835ca8bf204d20866c26d235c7641f7995
jombi <jombi@duck.com>
2025-04-29 14:45:01 +0900

    fix :: remove jackson redis serializer to correct serialize

--- adapter-persistence/src/main/kotlin/dev/orgsusu/adapterPersistence/database/config/SessionConfig.kt
+++ adapter-persistence/src/main/kotlin/dev/orgsusu/adapterPersistence/database/config/SessionConfig.kt
@@ -1,21 +1,13 @@
 package dev.orgsusu.adapterPersistence.database.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializer
 import org.springframework.security.core.session.SessionRegistry
-import org.springframework.security.jackson2.SecurityJackson2Modules
-import org.springframework.security.web.authentication.session.ChangeSessionIdAuthenticationStrategy
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository
 import org.springframework.session.data.redis.RedisIndexedSessionRepository
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession
 import org.springframework.session.security.SpringSessionBackedSessionRegistry
-import org.springframework.session.web.http.HeaderHttpSessionIdResolver
+import org.springframework.session.web.http.CookieHttpSessionIdResolver
 import org.springframework.session.web.http.HttpSessionIdResolver
 
 @Configuration
@@ -32,14 +24,6 @@ class SessionConfig {
     @Bean
     fun httpSessionIdResolver(): HttpSessionIdResolver = HeaderHttpSessionIdResolver.xAuthToken()
 
-    @Bean
-    fun springSessionDefaultRedisSerializer(): RedisSerializer<Any> {
-        val objectMapper = ObjectMapper()
-            .registerModule(KotlinModule.Builder().build())
-            .registerModule(JavaTimeModule())
-            .registerModules(SecurityJackson2Modules.getModules(ClassLoader.getSystemClassLoader()))
-        return GenericJackson2JsonRedisSerializer(objectMapper)
-    }
 
     @Bean
     fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
