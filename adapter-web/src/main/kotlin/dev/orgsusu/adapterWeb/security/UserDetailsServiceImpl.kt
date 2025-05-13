0c6d5295d1b14f9db8fa83b3dde5299131d55cbe
priverg163 <priverg163@dgsw.hs.kr>
2025-05-13 17:32:47 +0900

    refactor :: replace CustomException with UsernameNotFoundException

--- adapter-web/src/main/kotlin/dev/orgsusu/adapterWeb/security/UserDetailsServiceImpl.kt
+++ adapter-web/src/main/kotlin/dev/orgsusu/adapterWeb/security/UserDetailsServiceImpl.kt
@@ -1,17 +1,16 @@
 package dev.orgsusu.adapterWeb.security
 
-import dev.orgsusu.common.exception.CustomException
-import dev.orgsusu.application.exception.UserExceptionDetails
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 import dev.orgsusu.domain.port.outgoing.UserPort
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 
 @Service
 class UserDetailsServiceImpl(private val userPort: UserPort) : UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
         val user = userPort.findByCredential(username)
-            ?: throw CustomException(UserExceptionDetails.SESSION_USER_NOT_FOUND)
+            ?: throw UsernameNotFoundException("User with credential $username not found")
         return UserDetailsImpl(user)
     }
 }
