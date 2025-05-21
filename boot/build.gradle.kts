caf822470ef4f0412a2e195090c7fc30f659b4d9
jombi <jombi@duck.com>
2025-05-21 11:11:08 +0900

    refactor :: remove 'boot' submodule

--- boot/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    implementation(project(":adapter-web"))
-    implementation(project(":adapter-persistence"))
-
-    implementation(libs.spring.boot.core)
-    implementation(libs.spring.data.jpa)
-}
