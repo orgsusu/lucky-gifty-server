90817da70679d020e1b0d8b171c54196becb7088
priverg163 <priverg163@dgsw.hs.kr>
2025-05-30 09:57:45 +0900

    build :: add dependencies konvert, ksp, test, junit in adapter-toss

--- adapter-toss/build.gradle.kts
+++ adapter-toss/build.gradle.kts
@@ -7,4 +7,9 @@ dependencies{
 	implementation(project(":common"))
 
 	implementation(libs.spring.webflux)
+	implementation(libs.bundles.konvert.implementation)
+	ksp(libs.bundles.konvert.ksp)
+
+	testImplementation(libs.bundles.test)
+	testRuntimeOnly(libs.junit.platform)
 }
