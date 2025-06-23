198904e40e3faefe5a92332cd4619c5390ea3bd4
jombi <jombi@duck.com>
2025-06-23 14:57:22 +0900

    chore :: price range docs

--- domain/src/main/kotlin/dev/orgsusu/domain/kakao/model/consts/PriceRange.kt
+++ domain/src/main/kotlin/dev/orgsusu/domain/kakao/model/consts/PriceRange.kt
@@ -1,5 +1,14 @@
 package dev.orgsusu.domain.kakao.model.consts
 
+/**
+ * PriceRange
+ *
+ * 상품의 가격 범위 (단위: n만원)
+ *
+ * R_0_1: 1만원 미만
+ * R_1_3: 1만원 이상 3만원 미만
+ * ...
+ */
 enum class PriceRange {
     R_0_1,
     R_1_3,
